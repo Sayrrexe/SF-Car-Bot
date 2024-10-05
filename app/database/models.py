88258dc346a3a40e93f97745467d3f110b5d0ddc
wise <fomin27ivan@gmail.com>
2024-10-06 00:37:55 +0500

    DB fix

--- app/database/models.py
+++ app/database/models.py
@@ -1,5 +1,5 @@
 from tortoise.models import Model
-from tortoise import fields
+from tortoise import fields, on_delete
 
 from config import DB_URL
 
@@ -21,7 +21,7 @@ class User(Model):
 
 class Car(Model):
     id = fields.IntField(primary_key=True)
-    user = fields.ForeignKeyField('models.User', related_name='cars')
+    user = fields.ForeignKeyField('models.User', related_name='cars', on_delete=on_delete.CASCADE)
     brand = fields.CharField(max_length=255)
     model = fields.CharField(max_length=255)
     year = fields.IntField()
@@ -30,27 +30,27 @@ class Car(Model):
     
 class Purchases(Model):
     id = fields.IntField(primary_key=True)
-    user = fields.ForeignKeyField('models.User', related_name='purchases')
+    user = fields.ForeignKeyField('models.User', related_name='purchases', on_delete=on_delete.CASCADE)
     image = fields.TextField()
     text = fields.TextField()
     price = fields.DecimalField(max_digits=16, decimal_places=2)
     
 class Notes(Model):
     id = fields.IntField(primary_key=True)
-    user = fields.ForeignKeyField('models.User', related_name='notes')
+    user = fields.ForeignKeyField('models.User', related_name='notes', on_delete=on_delete.CASCADE)
     created_date = fields.DatetimeField(auto_now = True)
     price = fields.DecimalField(max_digits=10, decimal_places=2)
     title = fields.CharField(max_length=255)
     
 class Reminders(Model):
     id = fields.IntField(primary_key=True)
-    user = fields.ForeignKeyField('models.User', related_name='reminders')
+    user = fields.ForeignKeyField('models.User', related_name='reminders', on_delete=on_delete.CASCADE)
     created_at = fields.DatetimeField(auto_now_add=True)
     total_date = fields.DatetimeField()
     text = fields.TextField()
     
 class Service(Model):
     id = fields.IntField(pk=True)
-    car = fields.ForeignKeyField('models.Car', related_name='services')
+    car = fields.ForeignKeyField('models.Car', related_name='services', on_delete=on_delete.CASCADE)
     type = fields.CharField(max_length=2, choices=TYPE_CHOICES)
     date = fields.DatetimeField(auto_now = True)
