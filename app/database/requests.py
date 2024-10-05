4892888b7937af4e9ec46baab804549669e4047a
wise <fomin27ivan@gmail.com>
2024-10-06 00:31:04 +0500

    added create_user

--- app/database/requests.py
+++ app/database/requests.py
@@ -1,1 +1,6 @@
-# запросы к БД
+from app.database.models import User
+
+
+async def create_user(tg_id: int, username: str = None, ):
+    await User.create(username=username, tg_id=tg_id)
+    return
--- app/user.py
+++ app/user.py
@@ -2,6 +2,7 @@ from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
 
+from app.database.requests import create_user
 # для основной пользовательской части
 
 user = Router()
@@ -10,4 +11,5 @@ user = Router()
 
 @user.message(CommandStart())
 async def cmd_start(message: Message):
+    await create_user(message.from_user.id, message.from_user.username) # добавление пользователя в бд
     await message.answer('Добро пожаловать в бот!')
