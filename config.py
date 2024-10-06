23a35e32df43d99660b326cab940f6c9ace13d6f
wise <fomin27ivan@gmail.com>
2024-10-06 18:43:55 +0500

    code review

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 TOKEN='8086979841:AAHJbYtgpUzBfMaz4_GnL0WcoVE1Oc2h3BA'
 DB_URL='sqlite://db.sqlite3'
-
+TYPE_CHOICES = [
+        ('OL', 'OIL'), # масло
+        ('FL', 'filter'), # фильтр
+        ('SP', 'Support'), # тормозные колодки
+        ('FS', 'Full Service'), # полное ТО
+    ]
 # t.me/sf_car_bot
