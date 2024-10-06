389e2f88601f16366f2d1c6f5db4db7c0e11d0ed
wise <fomin27ivan@gmail.com>
2024-10-06 20:02:55 +0500

    добавлен профиль пользователя

--- app/user.py
+++ app/user.py
@@ -3,7 +3,7 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
 from aiogram.fsm.context import FSMContext
 
-from app.database.requests import create_user, create_car
+from app.database.requests import create_user, create_car, get_all_user_cars, get_all_user_nots_per_year
 import app.keyboards as kb
 import app.states as st
 # для основной пользовательской части
@@ -55,10 +55,31 @@ async def create_auto_model(message: Message, state: FSMContext):
 @user.message(st.CreateAutoFSM.engine)
 async def create_auto_engine(message: Message, state: FSMContext):
     await state.update_data(engine = message.text)
-    await message.answer('Почти у цели!\nВведите пробег вашего авто', reply_markup=kb.return_kb)
+    await message.answer('Почти у цели!\nВведите пробег вашего авто в киллометрах(укажите только число)', reply_markup=kb.return_kb)
     await state.set_state(st.CreateAutoFSM.mileage)
     
 @user.message(st.CreateAutoFSM.mileage)
 async def create_auto_mileage(message: Message, state: FSMContext):
     await state.update_data(mileage = message.text)
     await create_car(data = await state.get_data())
+    await state.clear()
+    await message.answer('Автомобиль добавлен!', reply_markup=kb.main_kb)
+    
+@user.message(Command('menu'))
+async def menu_cmd(message: Message):
+    await message.answer('Вы в главном меню!\nВыберите действие используя встроенную клавиатуру', reply_markup= kb.main_kb)
+    
+@user.message(F.text == 'Профиль')
+async def profile_cmd(message: Message):
+    car = await get_all_user_cars(message.from_user.id)
+    expenses = await get_all_user_nots_per_year(message.from_user.id)
+    cars = f'Автомобили:\n'
+    if car:  
+        for car in car:
+            brand = car['brand']
+            model = car['model']
+            year = car['year']
+            cars += f'- {brand} {model} | год выпуска: {year}\n'
+    else:
+        cars = 'У пользователя нет автомобилей.\n'
+    await message.answer(f'Профиль пользователя: {message.from_user.username}\n\n{cars}\nТраты на автомобиль за год: {expenses}', reply_markup=await kb.profile_kb(message.from_user.id))
