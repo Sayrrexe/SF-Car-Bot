25a77f5a368e3b995046176f80e450e7130c3bdc
Mikhail Bakushin <mikh96@icloud.com>
2024-10-12 15:18:08 +0500

    Upd config and req for working with postgres

--- config.py
+++ config.py
@@ -6,6 +6,11 @@ load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 DB_URL = os.getenv("DB_URL")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("DB_NAME")
 
 TYPE_CHOICES = [
     ("OL", "OIL"),  # масло
@@ -14,16 +19,39 @@ TYPE_CHOICES = [
     ("FS", "Full Service"),  # полное ТО
 ]
 
+# # sqlite
+# TORTOISE_ORM = {
+#     "connections": {
+#         "default": DB_URL,
+#     },
+#     "apps": {
+#         "models": {
+#             "models": ["app.database.models", "aerich.models"],
+#             "default_connection": "default",
+#         },
+#     },
+# }
+
+# postgres
 TORTOISE_ORM = {
     "connections": {
-        "default": DB_URL,
+        "default": {
+            "engine": "tortoise.backends.asyncpg",
+            "credentials": {
+                "host": DB_HOST,
+                "port": DB_PORT,
+                "user": DB_USER,
+                "password": DB_PASS,
+                "database": DB_NAME,
+            }
+        }
     },
     "apps": {
         "models": {
             "models": ["app.database.models", "aerich.models"],
             "default_connection": "default",
-        },
-    },
+        }
+    }
 }
 
 # t.me/sf_car_bot
